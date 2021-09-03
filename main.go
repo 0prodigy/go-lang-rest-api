07169d3ed7051e966540ce154bf1edcd79ae7cf1
Akash Pathak <pathakvikash9211@gmail.com>
2021-09-04 00:30:44 +0530

    Added upload route

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,12 +11,24 @@ import (
 
 func main() {
 	router := gin.Default()
-    router.GET("/hello", helloWorld)
-
+    router.GET("/hello/:id", helloWorld)
+	router.POST("/upload",handleUpload)
     router.Run("localhost:8080")
 }
 
+func handleUpload(c *gin.Context){
+	file,_ := c.FormFile("file")
+	filename := filepath.Base(file.Filename)
+	if err := c.SaveUploadedFile(file, filename); err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("upload file err: %s", err.Error()))
+		return
+	}
+	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
+}
+
 func helloWorld(c *gin.Context) {
+		id := c.Param("id")
+		fmt.Println(id)
 		fmt.Println("Hello World")
 	
 }
